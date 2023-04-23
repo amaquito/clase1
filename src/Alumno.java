f6eed6b4efee258ab7894b4a4724a8f190d53985
amaquito <inf.arturomaquito@gmail.com>
2023-04-22 19:59:56 -0500

    comentario4

--- src/Alumno.java
+++ src/Alumno.java
@@ -12,6 +12,30 @@ public class Alumno {
         this.apellido = apellido;
     }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
     public void mostrarNombre(){
         System.out.println("Hola, soy alumno y se decir mi nombre");
 
--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,16 @@
 
         Alumno alu1 = new Alumno();
         Alumno alu2 = new Alumno(5,"Edward","Valladares");
+
+        System.out.println("El Alumno " + alu2.getNombre() + " tiene como ID " + alu2.getId() );
+
+        alu1.setId(35);
+        alu1.setApellido("Maquito");
+        alu1.setNombre("Arturo");
+        System.out.println("__________________________");
+        System.out.println("El Alumno " + alu1.getNombre() + " tiene como ID " + alu1.getId() );
+        System.out.println("----------------------------------------");
+        alu2.setId(40);
+        System.out.println("El Alumno " + alu2.getNombre() + " tiene como ID " + alu2.getId() );
     }
 }
