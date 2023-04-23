ff563f9e62b751fdc72bd804dda628c54a799d93
amaquito <inf.arturomaquito@gmail.com>
2023-04-22 19:42:50 -0500

    comentario3

--- src/Alumno.java
+++ src/Alumno.java
@@ -3,10 +3,20 @@ public class Alumno {
     String nombre;
     String apellido;
 
+    public Alumno() {
+    }
+
+    public Alumno(int id, String nombre, String apellido) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
     public void mostrarNombre(){
         System.out.println("Hola, soy alumno y se decir mi nombre");
 
     }
+
     public void saberAprobado (double calficiacion){
         if(calficiacion >= 6){
             System.out.println("Aprobe la materia");
